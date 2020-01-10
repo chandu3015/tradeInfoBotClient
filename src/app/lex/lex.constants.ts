8bb9960310bde7e4d39cdf4f91c4af2a4fb51e1c
chandu3015 <40059088+chandu3015@users.noreply.github.com>
2020-01-10 13:25:54 -0600

    Security issue fix

--- src/app/lex/lex.constants.ts
+++ src/app/lex/lex.constants.ts
@@ -3,8 +3,8 @@ export  class LexConstants {
   //public static BOT_NAME = 'RestBotTest';
   public static BOT_NAME = 'POC';
   public static USER_ID = 'user';
-  public static AccessKey_Id = 'AKIAQCKD2NNI4QYJ6CUF';
-  public  static SecretAccess_Key = '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';
+  public static AccessKey_Id = '';
+  public  static SecretAccess_Key = '';
   public  static Region = 'us-east-1';
 
 }
